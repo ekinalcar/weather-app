6a44fd80d4680145e26b9686bdc8c9ca12d739a3
ekinalcar <ekinalcar@firmasoft.com>
2019-06-14 13:52:16 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Titles from './components/Titles'
+import Form from './components/Form'
+import Weather from './components/Weather'
 
-export default App;
+const API_KEY = '655b7faf408b249215abc299bf71cbc2';
+
+export default class App extends React.Component {
+
+  state = {
+    temperature:'',
+    city:'',
+    country:'',
+    humidity:'',
+    description:'',
+    error:''
+  }
+
+  handleWeather = async (e) => {
+     e.preventDefault();
+     const city = e.target.elements.city.value;
+     const country = e.target.elements.country.value;
+     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+     const data = await api_call.json();
+
+     if(city && country){
+       this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+       });
+     }
+  }
+
+  render(){
+    return (
+      <div>
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-sm-6 title_container">
+                  <Titles />
+                </div>
+                <div className="col-sm-6 form_container">
+                  <Form getWeather={this.handleWeather} />
+                  <Weather
+                    temperature={this.state.temperature}
+                    humidity={this.state.humidity}
+                    city={this.state.city}
+                    country={this.state.country}
+                    description={this.state.description}
+                    error={this.state.error}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+    )
+  }
+}
---
+++ src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Titles = () => (
+	<div>
+		<h1 className="title_container_title">Weather Finder</h1>
+		<h3 className="title_container_subtitle">Find out temperature, conditions and more...</h3>
+	</div>
+);
+
+export default Titles;
